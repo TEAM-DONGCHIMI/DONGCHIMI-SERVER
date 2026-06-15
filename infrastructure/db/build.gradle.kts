35b762fd7ae1cdd715efd4c29e3dab08d52defd9
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-06-15 23:34:26 +0900

    refactor: 인증 관련 의존성을 gateway-auth로, h2console을 infrastructure:db로 이동

--- infrastructure/db/build.gradle.kts
+++ infrastructure/db/build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.springframework.boot:spring-boot-h2console")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
