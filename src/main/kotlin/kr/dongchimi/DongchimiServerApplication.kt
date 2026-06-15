090b61364db9dcc49777493d145710cc9fa6e85b
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-06-15 17:17:52 +0900

    init: 프로젝트 초기 세팅

---
+++ src/main/kotlin/kr/dongchimi/DongchimiServerApplication.kt
@@ -0,0 +1,11 @@
+package kr.dongchimi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongchimiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DongchimiServerApplication>(*args)
+}
---
+++ src/test/kotlin/kr/dongchimi/DongchimiServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.dongchimi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DongchimiServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
