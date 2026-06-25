313427c48bdedd19a35fbbcdd6c3543aef24c31e
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-06-25 19:48:13 +0900

    chore: data class 빈 scope 제거

--- api/src/main/kotlin/kr/dongchimi/api/common/dto/PageResponse.kt
+++ api/src/main/kotlin/kr/dongchimi/api/common/dto/PageResponse.kt
@@ -3,5 +3,4 @@ package kr.dongchimi.api.common.dto
 data class PageResponse<T>(
     val content: List<T>,
     val hasNext: Boolean,
-) {
-}
+)
--- core/src/main/kotlin/kr/dongchimi/core/common/PageResult.kt
+++ core/src/main/kotlin/kr/dongchimi/core/common/PageResult.kt
@@ -3,5 +3,4 @@ package kr.dongchimi.core.common
 data class PageResult<T>(
     val content: List<T>,
     val hasNext: Boolean,
-) {
-}
+)
