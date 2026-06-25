4d27832f80fa1adf544e4a9d96c0443b68f667e4
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-06-26 02:35:40 +0900

    feat: ErrorCode 계층 구조 및 CoreException 추가

--- core/src/main/kotlin/kr/dongchimi/core/common/exception/CommonErrorCode.kt
+++ core/src/main/kotlin/kr/dongchimi/core/common/exception/CommonErrorCode.kt
@@ -1,4 +1,12 @@
 package kr.dongchimi.core.common.exception
 
-enum class CommonErrorCode {
+import kr.dongchimi.common.exception.ErrorStatus
+
+enum class CommonErrorCode(
+    override val status: Int,
+    override val message: String,
+) : ErrorCode {
+    INVALID_INPUT(ErrorStatus.BAD_REQUEST, "유효하지 않은 입력값입니다."),
+    UNAUTHORIZED(ErrorStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    INTERNAL_SERVER_ERROR(ErrorStatus.INTERNAL_SERVER_ERROR, "내부 서버 오류입니다. 다시 시도해 주세요."),
 }
--- core/src/main/kotlin/kr/dongchimi/core/common/exception/CoreException.kt
+++ core/src/main/kotlin/kr/dongchimi/core/common/exception/CoreException.kt
@@ -1,4 +1,13 @@
 package kr.dongchimi.core.common.exception
 
-class CoreException {
-}
+open class CoreException(
+    val errorCode: ErrorCode,
+    message: String = errorCode.message,
+    formatArgs: List<Any> = emptyList()
+): RuntimeException(
+    if (formatArgs.isEmpty()) {
+        message
+    } else {
+        message.format(*formatArgs.toTypedArray())
+    }
+)
