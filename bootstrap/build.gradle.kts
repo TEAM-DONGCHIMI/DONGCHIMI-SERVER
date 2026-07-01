765066be41b86de454539c63bc4c4fcf1904207d
imddoy <imddoy@duksung.ac.kr>
2026-07-02 00:21:37 +0900

    feat: Async JSON 구조화 로깅 설정 추가

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     implementation(project(":gateway:logging"))
     implementation(project(":infrastructure:db"))
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
