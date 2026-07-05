a674cad326b5a3d28ab398b21150ee46bfbc6912
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-07-05 16:37:15 +0900

    refactor: role api argument resolver 공통 로직을 core-api 베이스 클래스로 추출

--- api/admin-api/src/main/kotlin/kr/dongchimi/api/admin/resolver/AdminApiUserArgumentResolver.kt
+++ api/admin-api/src/main/kotlin/kr/dongchimi/api/admin/resolver/AdminApiUserArgumentResolver.kt
@@ -1,34 +1,17 @@
 package kr.dongchimi.api.admin.resolver
 
 import kr.dongchimi.api.admin.AdminApiUser
+import kr.dongchimi.api.core.resolver.RoleApiUserArgumentResolver
 import kr.dongchimi.core.auth.PrincipalProvider
 import kr.dongchimi.core.auth.Role
-import kr.dongchimi.core.common.exception.CommonErrorCode
-import kr.dongchimi.core.common.exception.CoreException
-import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.support.WebDataBinderFactory
-import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.method.support.ModelAndViewContainer
 
 @Component
 class AdminApiUserArgumentResolver(
-    private val principalProvider: PrincipalProvider,
-) : HandlerMethodArgumentResolver {
-    override fun supportsParameter(parameter: MethodParameter): Boolean = parameter.parameterType == AdminApiUser::class.java
-
-    override fun resolveArgument(
-        parameter: MethodParameter,
-        mavContainer: ModelAndViewContainer?,
-        webRequest: NativeWebRequest,
-        binderFactory: WebDataBinderFactory?,
-    ): AdminApiUser {
-        if (Role.ADMIN.name !in principalProvider.roles) throw CoreException(CommonErrorCode.FORBIDDEN)
-
-        return AdminApiUser(
-            principalProvider.userId,
-            principalProvider.roles,
-        )
-    }
-}
+    principalProvider: PrincipalProvider,
+) : RoleApiUserArgumentResolver<AdminApiUser>(
+        principalProvider,
+        Role.ADMIN,
+        AdminApiUser::class.java,
+        ::AdminApiUser,
+    )
--- api/user-api/src/main/kotlin/kr/dongchimi/api/user/resolver/UserApiUserArgumentResolver.kt
+++ api/user-api/src/main/kotlin/kr/dongchimi/api/user/resolver/UserApiUserArgumentResolver.kt
@@ -1,34 +1,17 @@
 package kr.dongchimi.api.user.resolver
 
+import kr.dongchimi.api.core.resolver.RoleApiUserArgumentResolver
 import kr.dongchimi.api.user.UserApiUser
 import kr.dongchimi.core.auth.PrincipalProvider
 import kr.dongchimi.core.auth.Role
-import kr.dongchimi.core.common.exception.CommonErrorCode
-import kr.dongchimi.core.common.exception.CoreException
-import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.support.WebDataBinderFactory
-import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.method.support.HandlerMethodArgumentResolver
-import org.springframework.web.method.support.ModelAndViewContainer
 
 @Component
 class UserApiUserArgumentResolver(
-    private val principalProvider: PrincipalProvider,
-) : HandlerMethodArgumentResolver {
-    override fun supportsParameter(parameter: MethodParameter): Boolean = parameter.parameterType == UserApiUser::class.java
-
-    override fun resolveArgument(
-        parameter: MethodParameter,
-        mavContainer: ModelAndViewContainer?,
-        webRequest: NativeWebRequest,
-        binderFactory: WebDataBinderFactory?,
-    ): UserApiUser {
-        if (Role.USER.name !in principalProvider.roles) throw CoreException(CommonErrorCode.FORBIDDEN)
-
-        return UserApiUser(
-            principalProvider.userId,
-            principalProvider.roles,
-        )
-    }
-}
+    principalProvider: PrincipalProvider,
+) : RoleApiUserArgumentResolver<UserApiUser>(
+        principalProvider,
+        Role.USER,
+        UserApiUser::class.java,
+        ::UserApiUser,
+    )
