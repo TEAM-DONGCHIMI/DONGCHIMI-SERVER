489b9c5505815498ce14359bb44067e854ead84c
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-07-06 02:55:57 +0900

    chore: api 모듈에서 spring-boot-validation 의존성 제외

--- buildSrc/src/main/kotlin/dongchimi.api-module-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/dongchimi.api-module-conventions.gradle.kts
@@ -5,7 +5,9 @@ dependencies {
     "implementation"(project(":core"))
     "runtimeOnly"(project(":gateway:auth"))
     "implementation"(project(":gateway:logging"))
-    "implementation"("org.springframework.boot:spring-boot-starter-webmvc")
+    "implementation"("org.springframework.boot:spring-boot-starter-webmvc") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-validation")
+    }
     "implementation"("tools.jackson.module:jackson-module-kotlin")
     "implementation"("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
     "implementation"("io.github.oshai:kotlin-logging-jvm:7.0.3")
