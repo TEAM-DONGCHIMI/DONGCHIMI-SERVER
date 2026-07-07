58544e4b1ed2dccaa5ba4dc4592ddba556f37beb
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-07-07 23:08:19 +0900

    refactor: RefreshToken 생성자 개선

--- core/src/main/kotlin/kr/dongchimi/core/auth/RefreshTokenAppender.kt
+++ core/src/main/kotlin/kr/dongchimi/core/auth/RefreshTokenAppender.kt
@@ -12,9 +12,8 @@ class RefreshTokenAppender(
     ): RefreshToken =
         refreshTokenRepository.save(
             RefreshToken(
-                tokenId = issued.tokenId,
-                userId = userId,
-                expiresAt = issued.expiresAt,
+                issued,
+                userId,
             ),
         )
 }
