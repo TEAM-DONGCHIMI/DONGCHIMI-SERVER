f8143bff68bbe966fc979540f3b8b0804b1bf443
imddoy <imddoy@duksung.ac.kr>
2026-07-08 20:33:06 +0900

    fix: FlyerAppender에 누락된 @Transactional 추가

--- core/src/main/kotlin/kr/dongchimi/core/market/FlyerAppender.kt
+++ core/src/main/kotlin/kr/dongchimi/core/market/FlyerAppender.kt
@@ -1,11 +1,13 @@
 package kr.dongchimi.core.market
 
 import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
 
 @Component
 class FlyerAppender(
     private val flyerRepository: FlyerRepository,
 ) {
+    @Transactional
     fun updateQrCode(
         flyer: Flyer,
         qrCode: String,
