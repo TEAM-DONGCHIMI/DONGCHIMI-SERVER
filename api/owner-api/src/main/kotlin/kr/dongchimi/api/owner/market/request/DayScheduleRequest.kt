d706dd9d85bc275f712eb049d3c3f575ab33831d
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-07-08 16:48:32 +0900

    fix: DaySchedule 시간 검증 로직에서 open/close null 시 NPE 발생하는 버그 수정

--- api/owner-api/src/main/kotlin/kr/dongchimi/api/owner/market/request/DayScheduleRequest.kt
+++ api/owner-api/src/main/kotlin/kr/dongchimi/api/owner/market/request/DayScheduleRequest.kt
@@ -13,8 +13,8 @@ data class DayScheduleRequest(
     val close: String?,
 ) {
     fun toDaySchedule(): DaySchedule {
-        open?.let { validate(it.isNotBlank()) { "영업 시작 시각은 필수로 입력해 주세요." } }
-        close?.let { validate(it.isNotBlank()) { "영업 종료 시각은 필수로 입력해 주세요." } }
+        validate(!open.isNullOrBlank()) { "영업 시작 시각은 필수로 입력해 주세요." }
+        validate(!close.isNullOrBlank()) { "영업 종료 시각은 필수로 입력해 주세요." }
 
         return DaySchedule(openTime = parseTime(open!!), closeTime = parseTime(close!!))
     }
