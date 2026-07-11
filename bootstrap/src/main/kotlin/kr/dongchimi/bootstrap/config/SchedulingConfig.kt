52e724bbcb23fc6a28f43f14d890cc68909f5a80
Sumin Hwang <163857590+tnals0924@users.noreply.github.com>
2026-07-11 21:45:56 +0900

    chore: 조회수 flush 스케줄링 활성화 및 설정 추가

---
+++ bootstrap/src/main/kotlin/kr/dongchimi/bootstrap/config/SchedulingConfig.kt
@@ -0,0 +1,8 @@
+package kr.dongchimi.bootstrap.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+class SchedulingConfig
