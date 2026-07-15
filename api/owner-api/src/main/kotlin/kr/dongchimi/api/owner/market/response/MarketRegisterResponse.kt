535e57cc1492dd764738fe15f9994ef53d2bc259
imddoy <imddoy@duksung.ac.kr>
2026-07-15 19:42:44 +0900

    refactor: response 마트 정보 전부 내려주는 것으로 확장

--- api/owner-api/src/main/kotlin/kr/dongchimi/api/owner/market/response/MarketRegisterResponse.kt
+++
@@ -1,8 +0,0 @@
-package kr.dongchimi.api.owner.market.response
-
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class MarketRegisterResponse(
-    @Schema(description = "등록된 마트 아이디")
-    val marketId: Long,
-)
